45c6306fd64aef2c39a62c53b45fa6ac05e1f996
m3c-git <ayyoubentreprise@gmail.com>
2024-01-08 22:04:46 +0100

    add exo fetch 4

--- bre01-react-j6/exos/exos-fetch4/main.js
+++ bre01-react-j6/exos/exos-fetch4/main.js
@@ -1,68 +1,68 @@
 const apiURL = "https://jsonplaceholder.typicode.com/posts/";
 main()
 
+
 async function main()
 {
     const wrapper = document.querySelectorAll("article");
     const loader = document.createElement("p");
-    const title = document.querySelector("#post-title");
+    const posttitle = document.querySelector("#post-title");
     const postbody = document.querySelector("#post-body");
 
-
-    document.getElementById("contact-form").addEventListener("submit", async function(e) {
-            e.preventDefault();
-         
-            let data = new FormData(this);
-            console.log(data)
-         
-            let xhr = new XMLHttpRequest();
-
-
-
-            const body = JSON.stringify({
-                title : "Ceci est le titre de mon nouveau post",
-                body : "Ceci est le contenu de mon nouveau post",
-                userId: 1
-            });
+    let form = document.getElementById("contact-form");
+    form.addEventListener("submit", async function(e) {
+        e.preventDefault();
+     
+        let title = document.querySelector("#title");
+        let content = document.querySelector("#content");
         
-            const options = {
-                method: 'POST',
-                body: body,
-                headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-              }
-            };
-        
-        
-            loader.classList.add("loader")
-            loader.innerText = "Loading...";
-        /*     wrapper.append(loader);
-         */    try {
-        
-                const r = await  fetch(apiURL, options)            
-        
-                        if (r.ok) {
-                            const post = await r.json();
-                            loader.remove();
-                            console.log(data)
-                            title.innerText = post.title;
-                            postbody.innerText = post.body;
-        
-                            
-        
-                        } else {
-                            throw new Error('Erreur serveur', {cause: r});
-                        }
-                    } catch (e) {
-                        loader.innerText = 'Impossible de charger les articles...';
-                        loader.style.color = 'red';
-                        return;
-                    }
-        
-            return false;
-        });
-
+        console.log(title.value + " "+ content.value)
+     
     
+   
+    
+
+
+        const body = JSON.stringify({
+            title : title.value,
+            body : content.value,
+            userId: 1
+        });
+    
+        const options = {
+            method: 'POST',
+            body: body,
+            headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          }
+        };
+    
+    
+        loader.classList.add("loader")
+        loader.innerText = "Loading...";
+    /*     wrapper.append(loader);
+     */    try {
+    
+            const r = await  fetch(apiURL, options)            
+    
+                    if (r.ok) {
+                        const post = await r.json();
+                        loader.remove();
+                        console.log(post)
+                        posttitle.innerText = post.title;
+                        postbody.innerText = post.body;
+    
+                        
+    
+                    } else {
+                        throw new Error('Erreur serveur', {cause: r});
+                    }
+                } catch (e) {
+                    loader.innerText = 'Impossible de charger les articles...';
+                    loader.style.color = 'red';
+                    return;
+                }
+    });
 } 
 
         
